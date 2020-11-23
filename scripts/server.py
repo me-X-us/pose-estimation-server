713d8d375cc4376d16859211f06ef8af51f25895
always0ne <si8363@naver.com>
2020-11-23 18:39:23 +0900

    refactor: set contentType

--- scripts/server.py
+++ scripts/server.py
@@ -1,11 +1,13 @@
 from extract_poses import extract_poses
 from flask import Flask
 from flask import request
+from flask import Response
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return extract_poses(request.args['video_path'],False,False)
+    return Response(extract_poses(request.args['video_path'],False,False), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
